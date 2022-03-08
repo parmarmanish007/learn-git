a8c7ed60f1743fda7157a97509223f777b996c28
manish <manish.parmar@tntra.com>
2022-03-08 15:44:03 +0530

    added condition to execute from main file

--- exchange.py
+++ exchange.py
@@ -1,6 +1,9 @@
-a = int(input('enter element :'))
-b = int(input('enter element :'))
-a = a + b
-b = a - b
-a = a - b
-print(a, b)
+
+
+if __name__ == "__main__"
+    a = int(input('enter element :'))
+    b = int(input('enter element :'))
+    a = a + b
+    b = a - b
+    a = a - b
+    print(a, b)
